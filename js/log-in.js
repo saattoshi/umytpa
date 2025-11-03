23a30260002a2f632e7b6474f6e150a2fa39cd3f
H1ruka <alisherkanadil@gmail.com>
2025-11-03 11:36:56 +0500

    testing input in login

--- js/log-in.js
+++ js/log-in.js
@@ -1,5 +1,8 @@
 const btn = document.getElementById('log-in');
+const id = document.getElementById('id')
+
 
 btn.addEventListener('click', function(){
     window.location.href = "home.html";
+    console.log(id);
 })
