bf5f9ea9858f2aa556588163b597669febc6faf7
H1ruka <alisherkanadil@gmail.com>
2025-11-03 11:44:24 +0500

    added verification kinda stuff

--- js/log-in.js
+++ js/log-in.js
@@ -1,8 +1,13 @@
 const btn = document.getElementById('log-in');
-const id = document.getElementById('id')
+const id = document.querySelector('#id')
+const password = document.querySelector('#password')
 
 
 btn.addEventListener('click', function(){
-    window.location.href = "home.html";
-    console.log(id);
+    if (id.value == 250118021 && password.value == "Id080112554005"){
+        window.location.href = "home.html";
+    }else{
+        alert("ID or Password is incorrect")
+    }
+    
 })
