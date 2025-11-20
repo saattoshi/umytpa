def6837eddce5f3f8785782f1b2a0df9d4468606
H1ruka <alisherkanadil@gmail.com>
2025-11-20 14:43:24 +0500

    auth js somehow wroking

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,26 @@
-function getData(endpoint){
-    return fetch(endpoint);
+let btn = document.querySelector('#log-in');
+const id = document.querySelector('#id')
+const password = document.querySelector('#password')
+console.log(btn);
+
+async function getUser() {
+    const res = await fetch('db.json')
+    const data = await res.json()
+    return data
 }
 
-getData('db.json')
-    .then((dataset) => dataset.json())
-    .then((dataArr) => dataArr);
+const users = getUser();
+
+function userList(users){
+    for (const user of users){
+        if (id.value === user.username && password.value === user.password){
+            // window.location.href = "home.html";
+            alert("Log in info correct")
+        }else{
+            alert("ID or Password is incorrect")
+        }
+    }
+}
 
 
+// btn.addEventListener('click', userList(users))
